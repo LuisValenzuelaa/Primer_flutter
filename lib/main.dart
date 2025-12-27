45e383f7fc1392751b991ec5c01a68a6cae24f9e
L-A-V-2-2 <lav80111@gmail.com>
2025-12-26 18:35:54 -0600

    se agregó una funcionalidad que permite agregar el nombre del usuario

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class HomeScreen extends StatefulWidget{
 
 class _HomeScreenState extends State<HomeScreen>{
   String mensajeDia = 'Stay Hard';
+  String nombre = '';
   bool estaCargando = true;
   final List<String> variaciones = ['Even today', 'You will', 'You can', 'Always', 'Friend, ', 'Time to', ''];
   @override
@@ -44,11 +45,18 @@ class _HomeScreenState extends State<HomeScreen>{
     }else{
       prefijo = prefs.getString('prefijo_del_dia') ?? '';
     }
-    setState((){
+    setState(() {
       mensajeDia = '$prefijo Stay Hard';
       estaCargando = true;
+      nombre = prefs.getString('nombre_del_usuario') ?? '';
     });
   }
+  Future<void> _cargarNombre() async {
+      final prefs = await SharedPreferences.getInstance();
+      setState((){
+        nombre = prefs.getString('nombre_del_usuario') ?? '';
+      });
+    }
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -62,28 +70,24 @@ class _HomeScreenState extends State<HomeScreen>{
             color: Colors.white,
             size: 32,
           ),
-          onPressed: (){
-            Navigator.push(
+          onPressed: () async{
+            final result = await Navigator.push(
               context,
               MaterialPageRoute(
                 builder: (context) => MenuScreen()
               ),
             );
+            if (result == true){
+              _cargarNombre();
+            }
           },
         ),
       ),
       body: Center(
-        child: estaCargando ? Text(
-          'Stay Hard',
-          style: TextStyle(
-            fontFamily: 'BBHBOGLE',
-            color: Colors.white,
-            fontSize: 28,
-            fontWeight: FontWeight.normal,
-            letterSpacing: 1.5,
-          ),
-          textAlign: TextAlign.center,
-        ) : Text(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          Text(
           mensajeDia,
           style: TextStyle(
             fontFamily: 'BBHBOGLE',
@@ -94,12 +98,37 @@ class _HomeScreenState extends State<HomeScreen>{
           ),
           textAlign: TextAlign.center,
         ),
+        Text(
+          nombre,
+          style: TextStyle(
+            fontFamily: 'BBHBOGLE',
+            fontSize: 28,
+            color: Colors.amber,
+            fontWeight: FontWeight.normal,
+            letterSpacing: 1.5,
+          ),
+        ),
+        ],)
       ),
     );
   }
 }
-class MenuScreen extends StatelessWidget{
+class MenuScreen extends StatefulWidget{
   const MenuScreen({super.key});
+  @override
+  State<MenuScreen> createState() => _MenuScreenState();
+}
+class _MenuScreenState extends State<MenuScreen>{
+  final TextEditingController controlador = TextEditingController();
+  late SharedPreferences prefs;
+  @override
+  void initState(){
+    super.initState();
+    _initPrefs();
+  }
+  Future<void> _initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+  }
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -107,26 +136,74 @@ class MenuScreen extends StatelessWidget{
       appBar: AppBar(
         backgroundColor: Colors.black,
         elevation: 0,
+        iconTheme: IconThemeData(color: Colors.white),
         title: Text(
           'Menu',
           style: TextStyle(
             fontFamily: 'BBHBOGLE',
-            color: Colors.white,
             fontSize: 28,
+            color: Colors.white,
           ),
         ),
       ),
       body: Center(
-        child: Text(
-          'Here goes the menu',
-          style: TextStyle(
-            fontFamily: 'BBHBOGLE',
-            color: Colors.white,
-            fontSize: 28,
-          ),
-          textAlign: TextAlign.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 300,
+              height: 60,
+                child: TextField(
+                style: TextStyle(
+                  fontFamily: 'BBHBOGLE',
+                  fontSize: 17,
+                  color: Colors.white,
+                ),
+                controller: controlador,
+                decoration: const InputDecoration(
+                  labelText: 'Write your name',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)
+                  ),
+                  labelStyle: TextStyle(
+                    fontFamily: 'BBHBOGLE',
+                    fontSize: 17,
+                    color: Colors.grey,
+                  )
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                side: BorderSide(color: Colors.white,),
+                textStyle: TextStyle(
+                  fontFamily: 'BBHBOGLE',
+                  fontSize: 20,
+                ),
+              ),
+              onPressed: () async{
+                await prefs.setString('nombre_del_usuario', controlador.text);
+                Navigator.pop(context, true);
+              },
+              child: const Text('Save'),
+            )
+          ],
         ),
       ),
     );
+  }
+  @override
+  void dispose(){
+    controlador.dispose();
+    super.dispose();
   }
 }
